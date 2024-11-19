1c09d86dba1ef6035e655e68914288fc61aa442f
Banu1-coder <banuteja.deshaboina@slu.edu>
2024-11-19 05:31:49 +0000

    Committed code in app.js

--- app.js
+++ app.js
@@ -18,5 +18,10 @@ app.get('/', api.handleRoot);
 app.get('/products', api.listProducts);
 app.get('/products/:id', api.getProduct);
 
-// Start the server
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+// Error-handling middleware
+app.use(middleware.handleError);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
