edd38a31e65913b359b8bd54dd56239152f0fd82
Banu1-coder <banuteja.deshaboina@slu.edu>
2024-11-19 02:10:29 +0000

    Updated api.js code.

--- api.js
+++ api.js
@@ -1,13 +1,14 @@
+// api.js
 const path = require('path');
-const fs = require('fs').promises; // Ensure fs is imported here for reading files
+const Products = require('./products');  // Import the Products service
 
 /**
  * Handle the root route
  * @param {object} req
  * @param {object} res
  */
-function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html')); // Serves the HTML file
+function handleRoot (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 }
 
 /**
@@ -15,22 +16,17 @@ function handleRoot(req, res) {
  * @param {object} req
  * @param {object} res
  */
-async function listProducts(req, res) {
-  // Add CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Read the products file
-  const productsFile = path.join(__dirname, 'data/full-products.json');
-
+async function listProducts (req, res) {
   try {
-    const data = await fs.readFile(productsFile); // Read the JSON file asynchronously
-    res.json(JSON.parse(data)); // Parse and send the JSON response
+    // Use the Products service to get the list of products
+    const products = await Products.list();
+    res.json(products);  // Send the products as the response
   } catch (err) {
-    res.status(500).json({ error: err.message }); // Send error response
+    res.status(500).json({ error: err.message });
   }
 }
 
 module.exports = {
   handleRoot,
-  listProducts,
+  listProducts
 };
